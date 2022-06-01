eb126c4008fbb901c114b2fdce75aeb428a76390
Johnson Anna (s127980) <anna.johnson@student.ap.be>
2022-06-01 20:17:18 +0200

    guitars Server applications

--- Fender/Index.ts
+++ Fender/Index.ts
@@ -1,3 +1,8 @@
+import express from 'express';
+import ejs from 'ejs';
+
+
+
 
 interface guitar {
     name: string,
@@ -5,3 +10,20 @@ interface guitar {
     wood: string[],
     NickelOrPb: boolean
 }
+
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("port",3000);
+app.get("/", (req,res) => {
+    res.render("index");
+});
+app.listen(app.get("port"), () => {
+    console.log(`Local url: http://localhost:${app.get("port")}`);
+
+});
+
+export{}
--- Martin/Index.ts
+++ Martin/Index.ts
@@ -1,3 +1,9 @@
+import express from 'express';
+import ejs from 'ejs';
+
+
+
+
 
 interface guitar {
     name: string,
@@ -5,3 +11,20 @@ interface guitar {
     wood: string[],
     NickelOrPb: boolean
 }
+
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("port",3000);
+app.get("/", (req,res) => {
+    res.render("index");
+});
+app.listen(app.get("port"), () => {
+    console.log(`Local url: http://localhost:${app.get("port")}`);
+
+});
+
+export{}
